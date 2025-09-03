e7d7c3dc66777664f356bf05bc4e2a5f94d5d817
Jasper Mare <jaspermare@icloud.com>
2025-09-03 13:51:46 +0100

    added test script

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        Debug.Log("hello world");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
