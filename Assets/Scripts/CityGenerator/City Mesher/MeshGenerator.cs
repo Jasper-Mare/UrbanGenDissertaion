818a868f84ffe4f845950f06fc3ba74e8176b877
Jasper Mare <jaspermare@icloud.com>
2026-02-18 13:02:01 +0000

    added city mesher

---
+++ Assets/Scripts/CityGenerator/City Mesher/MeshGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Mathematics;
+using Debug = UnityEngine.Debug;
+using IEnumerator = System.Collections.IEnumerator;
+using MonoBehaviour = UnityEngine.MonoBehaviour;
+
+namespace CityGenerator.MeshUtilities {
+    class MeshGenerator {
+
+        Random rng;
+
+        public MeshGenerator(uint randomSeed) {
+            rng = new Random(randomSeed);
+        }
+
+        public IEnumerator Run(MonoBehaviour runner) {
+
+            yield return null;
+            Debug.Log("");
+        }
+
+
+    }
+}
