43955ee9297249a212e70713c02656bf991fd133
Jasper Mare <jaspermare@icloud.com>
2026-03-04 17:03:27 +0000

    made better road mesh

--- Assets/Scripts/TestScripts/CityMesherTestScript.cs
+++ Assets/Scripts/TestScripts/CityMesherTestScript.cs
@@ -120,11 +120,41 @@ public class CityMesherTestScript : MonoBehaviour {
         template.maximumSteepness = bridgeMaximumSteepness;
         template.minimumIntersectionRadius = intersectionMinimumRadius;
         template.bridgingHeight = bridgeHeight;
+
+        /*
+
+        O=========O
+        |         |
+        O         O
+         \       /
+          O-----O
+
+        */
         template.outline = new OutlineShape(
-            new Vector2[] { new(1, 0), new(-1, 0) },
-            new Vector2[] { new(0, 1), new(0, 1) },
-            new float[] { 0.0f, 1.0f },
-            new int[] { 0, 1 }
+            new Vector2[] {
+                new(1, 0.1f), new(-1, 0.1f), // top edge
+                new(-1, 0.1f), new(-1, -0.3f), new(-0.75f, -0.5f), // left edge
+                new(-0.75f, -0.5f), new(0.75f, -0.5f), // bottom edge
+                new(0.75f, -0.5f), new(1, -0.3f), new(1, 0.1f), // right edge
+            },
+            new Vector2[] {
+                new(0, 1), new(0, 1), // top edge
+                new(-1, 0), new(-0.89f, -0.45f), new(-0.45f, -0.89f), // left edge
+                new(0, -1), new(0, -1), // bottom edge
+                new(0.45f, -0.89f), new(0.89f, -0.45f), new(1, 0), // right edge
+            },
+            new float[] {
+                0.0f, 0.5f, // top edge (road)
+                0.5f, 0.57f, 0.62f, // left edge (concrete)
+                0.62f, 0.88f, // bottom edge (concrete)
+                0.88f, 0.93f, 1.0f // right edge (concrete)
+            },
+            new int[] {
+                0,1, // top
+                2,3, 3,4, // left
+                5,6, // bottom
+                7,8, 8,9 // right
+            }
         );
         template.roadMaterial = templateMaterial;
 
