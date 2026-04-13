e2aec67ba191116be166925843c761033c6449a3
Jasper Mare <jaspermare@icloud.com>
2026-04-13 14:10:56 +0100

    updated outline used for bezier test scene

--- Assets/Scripts/TestScripts/BezierTestScript.cs
+++ Assets/Scripts/TestScripts/BezierTestScript.cs
@@ -15,12 +15,34 @@ public class TestScript : MonoBehaviour {
     [SerializeField]
     bool doUpdate = false;
 
+    System.Collections.Generic.List<GameObject> debugs = new();
+
     OutlineShape outline = new OutlineShape(
-        new Vector2[] { new(1, 0), new(-1, 0) },
-        new Vector2[] { new(0, 1), new(0, 1) },
-        new float[] { 0.0f, 1.0f  },
-        new int[] { 0, 1  }
-    );
+        new Vector2[] {
+            new(1, 0.1f), new(-1, 0.1f), // top edge
+            new(-1, 0.1f), new(-1, -0.3f), new(-0.75f, -0.5f), // left edge
+            new(-0.75f, -0.5f), new(0.75f, -0.5f), // bottom edge
+            new(0.75f, -0.5f), new(1, -0.3f), new(1, 0.1f), // right edge
+        },
+        new Vector2[] {
+            new(0, 1), new(0, 1), // top edge
+            new(-1, 0), new(-0.89f, -0.45f), new(-0.45f, -0.89f), // left edge
+            new(0, -1), new(0, -1), // bottom edge
+            new(0.45f, -0.89f), new(0.89f, -0.45f), new(1, 0), // right edge
+        },
+        new float[] {
+            0.0f, 0.5f, // top edge (road)
+            0.5f, 0.57f, 0.62f, // left edge (concrete)
+            0.62f, 0.88f, // bottom edge (concrete)
+            0.88f, 0.93f, 1.0f // right edge (concrete)
+        },
+        new int[] {
+            0,1, // top
+            2,3, 3,4, // left
+            5,6, // bottom
+            7,8, 8,9 // right
+        }
+        );
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
@@ -34,13 +56,21 @@ public class TestScript : MonoBehaviour {
 
         BezierCurve curve = new BezierCurve( splinePoints.Select(t => t.position).ToArray() );
         OrientedPoint[] points = curve.SampleOrientedPoints(sampleStep);
+
+        // delete old debugs
+        foreach (GameObject objDebug in debugs) {
+            Destroy(objDebug);
+        }
+        debugs.Clear();
+
         // debug oriented points
-        //foreach (OrientedPoint pt in points) {
-        //    GameObject objDebug = new GameObject($"debug{pt.GetHashCode()}");
-        //    objDebug.transform.parent = this.transform;
-        //    var debug = objDebug.AddComponent<OrientedPointDebug>();
-        //    debug.point = pt;
-        //}
+        foreach (OrientedPoint pt in points) {
+            GameObject objDebug = new GameObject($"debug{pt.GetHashCode()}");
+            objDebug.transform.parent = this.transform;
+            var debug = objDebug.AddComponent<OrientedPointDebug>();
+            debug.point = pt;
+            debugs.Add(objDebug);
+        }
 
         MeshCreator.ExtrudeMeshAlongOrientedPath(MeshObject.Mesh, outline, points);
 
