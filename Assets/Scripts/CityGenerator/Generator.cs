87e603825681daf8937be64fa220ce2f282e5f8e
Jasper Mare <jaspermare@icloud.com>
2026-04-15 13:10:53 +0100

    edited logging for generator

--- Assets/Scripts/CityGenerator/Generator.cs
+++ Assets/Scripts/CityGenerator/Generator.cs
@@ -73,16 +73,30 @@ namespace CityGenerator {
 
         public IEnumerator Run(MonoBehaviour runner, bool showDebug) {
 
+            if (showDebug) {
+                Debug.Log("CityGenerator: Begining Generation");
+                Debug.Log("CityGenerator: Running TensorFieldGenerator");
+            }
+
             TensorField field = TensorFieldGenerator.Generate(position, size, numberOfTensors, numIterations, decayConstant, seed);
+            yield return null;
+
             if (showDebug) {
                 MeshCreator.CreatePlane(mesh, size.x, size.y, 1, 1);
                 field.Visualise(visualiserMaterial);
+
+                Debug.Log("CityGenerator: TensorFieldGenerator Completed");
+                Debug.Log("CityGenerator: Running HyperStreamlineGenerator");
             }
-            yield return null;
 
             streamlineGenerator = new HyperStreamlineGenerator(field, maxLength, minSeperation, lookAheadDist, seedDensity, seed);
             yield return runner.StartCoroutine(streamlineGenerator.Run(runner));
             yield return null;
+
+            if (showDebug) {
+                Debug.Log("CityGenerator: HyperStreamlineGenerator Completed");
+                Debug.Log("CityGenerator: Running BridgeDesignator");
+            }
 
             List<HyperStreamline> streamlines = new List<HyperStreamline>();
             streamlines.AddRange(streamlineGenerator.majorStreamlines);
@@ -92,12 +106,22 @@ namespace CityGenerator {
             yield return runner.StartCoroutine(bridger.Run(runner));
             yield return null;
 
+            if (showDebug) {
+                Debug.Log("CityGenerator: BridgeDesignator Completed");
+                Debug.Log("CityGenerator: Running CityMeshGenerator");
+            }
+
             List<HyperStreamlineIntersection> intersections = bridger.intersections;
             List<Bridge> bridges = bridger.bridges;
 
             meshGenerator = new CityMeshGenerator(seed, template, streamlines, intersections, bridges);
             yield return runner.StartCoroutine(meshGenerator.Run(runner));
             CityRoot = meshGenerator.CityRoot;
+
+            if (showDebug) {
+                Debug.Log("CityGenerator: CityMeshGenerator Completed");
+                Debug.Log("CityGenerator: Completed Generation");
+            }
         }
 
         public void DebugDraw() {
