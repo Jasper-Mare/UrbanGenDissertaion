19121c46db4516bc2fdb4be0249a29f9aa3a096f
Jasper Mare <jaspermare@icloud.com>
2026-04-20 15:28:08 +0100

    made roadlines flat

--- Assets/Scripts/CityGenerator/StreetGraph/HyperStreamline.cs
+++ Assets/Scripts/CityGenerator/StreetGraph/HyperStreamline.cs
@@ -37,7 +37,7 @@ namespace CityGenerator.StreetGraph {
                 Debug.DrawRay(new Vector3(points[0].x, 0, points[0].y), Vector3.up * 3, Color.white);
                 for (int i = 1; i < points.Count; i++) {
                     Debug.DrawRay(new Vector3(points[i].x, 0, points[i].y), Vector3.up, Color.white);
-                    Debug.DrawLine(new Vector3(points[i].x, i*0.01f, points[i].y), new Vector3(points[i-1].x, 0.01f * (i-1), points[i-1].y),
+                    Debug.DrawLine(new Vector3(points[i].x, 0.25f, points[i].y), new Vector3(points[i-1].x, 0.25f, points[i-1].y),
                         Color.HSVToRGB(math.abs(math.sin(PreviousDirection.x) * math.cos(PreviousDirection.y + 1)) % 1, 1, 1)
                     );
                 }
